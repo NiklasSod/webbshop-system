fee2f66cf5ffbbd5bc889d37d451ceb1b52801c7
Georgios <test@example.com>
2021-05-11 14:08:28 +0200

    add header menu

--- views/include/header.php
+++ views/include/header.php
@@ -1,14 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
   <link rel="stylesheet" href="./styles/bootstrap.css">
   <link rel="stylesheet" href="./styles/styles.css">
+  <title>webbshop-system</title>
 </head>
+
 <body>
-    <div class="container">
-      <div class="row">
-    
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Magic-Cards</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Shop <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Profile</a>
+        </li>
+
+        <div>
+          <li class="nav-item">
+            <a class="nav-link text-center" href="#">webbshop-system</a>
+          </li>
+        </div>
+
+      </ul>
+      <span class="navbar-text">
+        Sign in / up
+      </span>
+    </div>
+  </nav>
+
+
+  <a href="index.php">
+    <h1 class="text-center">webbshop-system</h1>
+  </a>
+  <div class="container">
+    <div class="row">
