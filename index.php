03d043c9bee165cda9ff649e488013d56cf9992a
Niklas Söderberg <nma.prob@gmail.com>
2021-05-24 11:56:16 +0200

    add another controller

--- index.php
+++ index.php
@@ -4,10 +4,12 @@ require_once("models/Database.php");
 require_once("models/Model.php");
 require_once("views/View.php");
 require_once("./controllers/Controller.php");
+require_once("./controllers/AdminController.php");
 
 $database = new Database("webshop2", "root", "root");
 $model = new Model($database);
 $view = new View();
-$controller = new Controller($model, $view);
+$adminController = new AdminController($model, $view);
+$controller = new Controller($model, $view, $adminController);
 
 $controller->main();
