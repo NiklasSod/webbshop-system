04cc7fd54a48330f66ab2ed0d1530fa8e8a83176
Niklas Söderberg <nma.prob@gmail.com>
2021-05-24 18:01:56 +0200

    styling detailorderpage

--- views/include/orderdetailpage.php
+++ views/include/orderdetailpage.php
@@ -1,27 +1,29 @@
 <?php
 
-// foreach ($orders as $order) {
-//     viewOneOrderDetail($order);
-// }
+echo "<h3 class='col-md-12 mx-auto text-center mb-5'>Order Id " . $_GET['id'] . "</h3>";
 
-// function viewOneOrderDetail($order) {
+foreach ($orders as $order) {
+    viewOneOrderDetail($order);
+}
+
+function viewOneOrderDetail($order) {
         
-//         $html = <<<HTML
+        $html = <<<HTML
         
-//         <div class="col-md-5 mx-auto">
-//             <p></p>
-//             <p></p>
-//             <p></p>
-//         </div>
+        <div class="col-md-5 mx-auto border border-primary mb-4">
+            <p>Product Id: $order[productId]</p>
+            <p>Amount: $order[amount]</p>
+            <p>Price: $order[price]</p>
+        </div>
 
-//         HTML;
+        HTML;
 
-//         echo $html;
-// }
+        echo $html;
+}
 
-echo "<pre>";
-print_r($orders);
-echo "</pre>";
+// echo "<pre>";
+// print_r($orders);
+// echo "</pre>";
 
 // echo "<pre>";
 // print_r($_SESSION);
